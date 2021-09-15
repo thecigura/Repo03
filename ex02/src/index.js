987d774d9396ec192476accdaac14e79e0367061
Denan <denan.cigura@gmail.com>
2021-09-15 21:06:51 +0200

    Added Exercise 02 - JS commenting inside index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+
+    //  Hello STORM, this is single-line JS comment
+    /*
+        Hello STORM, this is multi-line JS comment
+    */
+
+    // Only change code above this line
+
+}
+
+main();
+module.exports = main;
