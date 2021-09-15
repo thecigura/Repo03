e54d474124d8091d323a4b83caa52a4d2b862b91
Denan <denan.cigura@gmail.com>
2021-09-15 22:01:03 +0200

    Added Exercise 07 - JS Case Sensitivity

---
+++ ex07/src/index.js
@@ -0,0 +1,22 @@
+function main() {
+    // Only change code below this line
+    // Variable declaration
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Variable assignment
+    var studlyCapVar = 10;
+    var properCamelCase = "A string";
+    var titleCaseOver = 9000;
+
+    // Only change code above this line
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver  
+    }
+}
+
+console. log (main() );
+module.exports = main;
